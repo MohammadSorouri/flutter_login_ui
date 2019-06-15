c13b90590dfa71e69e196aa74d370574fcd55060
Mohammad Sorouri <sorouri1380@gmail.com>
2019-06-15 23:44:10 +0430

    add hero

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 class LoginPage extends StatefulWidget {
+  static String tag = 'login-page';
   @override
   _LoginPageState createState() => _LoginPageState();
 }
@@ -7,6 +8,14 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
+    final logo = Hero(
+      tag: "hero",
+      child: CircleAvatar(
+        backgroundColor: Colors.transparent,
+        radius: 48,
+        child: Image(image: AssetImage("assets/logo.png")),
+      ),
+    );
     return Container();
   }
 }
