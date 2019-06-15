76ecb7d865cb72add0b0e8fc6e775799d881409b
Mohammad Sorouri <sorouri1380@gmail.com>
2019-06-16 00:06:39 +0430

    add item in login_page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -16,6 +16,44 @@ class _LoginPageState extends State<LoginPage> {
         child: Image(image: AssetImage("assets/logo.png")),
       ),
     );
+    final email = TextField(
+      keyboardType: TextInputType.emailAddress,
+      autofocus: false,
+      decoration: InputDecoration(
+        hintText: "Email",
+        contentPadding: EdgeInsets.only(left: 20,top: 10,right: 20,bottom: 10),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(32)
+        )
+      ),
+    );
+    final password = TextField(
+      obscureText: true,
+      autofocus: false,
+      decoration: InputDecoration(
+          hintText: "Password",
+          contentPadding: EdgeInsets.only(left: 20,top: 10,right: 20,bottom: 10),
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(32)
+          )
+      ),
+    );
+    final loginButton = Material(
+      shadowColor: Colors.lightBlue.shade100,
+      elevation: 5,
+      borderRadius: BorderRadius.circular(30),
+      child: MaterialButton(onPressed: (){},
+        minWidth: 200,
+        height: 42,
+        color: Colors.lightBlueAccent,
+        child: Text("Log In",style: TextStyle(color: Colors.white),),
+      ),
+    );
+    final forgotLabel = FlatButton(
+      child: Text("Forgot Password?",style: TextStyle(color: Colors.black54),),
+
+    );
+
     return Container();
   }
 }
